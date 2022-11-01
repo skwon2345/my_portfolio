bacf0afc0895e84caac5d9aa642566dd6db8ba63
sukkwono <sukkwono@sfu.ca>
2022-11-01 03:09:55 -0700

    include all components to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
 
+import {About, Contact, Experience, Footer, Header, Nav, Portfolio, Services, Testimonials} from './components';
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
