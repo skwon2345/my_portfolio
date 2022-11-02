3e304ce4fc6767ddba68b01b6be79fdd9ef245a1
sukkwono <sukkwono@sfu.ca>
2022-11-01 23:44:23 -0700

    add person image

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hello I'm</h5>
+        <h1>Sukkwon On</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+      </div>
+
+    </header>
   )
 }
 
