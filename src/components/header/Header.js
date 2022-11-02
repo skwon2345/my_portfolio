39cf6d27eb1aa8380968b6931f17460310dee239
sukkwono <sukkwono@sfu.ca>
2022-11-02 00:10:51 -0700

    add HeaderSocials to Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css';
 import CTA from './CTA';
 import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
@@ -11,12 +12,13 @@ const Header = () => {
         <h1>Sukkwon On</h1>
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
+        <HeaderSocials />
 
-        <div className="me">
+        {/* <div className="me">
           <img src={ME} alt="me" />
-        </div>
+        </div> */}
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
-
     </header>
   )
 }
