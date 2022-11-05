0b2683cab84b24f0befa8f240f9aa478671e6fdc
sukkwono <sukkwono@sfu.ca>
2022-11-05 16:37:14 -0700

    delete Scroll Down from Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -13,11 +13,6 @@ const Header = () => {
         <h5 className='text-light'>Fullstack Developer</h5>
         <CTA />
         <HeaderSocials />
-
-        {/* <div className="me">
-          <img src={ME} alt="me" />
-        </div> */}
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
