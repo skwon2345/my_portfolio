492eefd1b8d5d5df0e76439205241d5aad8b462c
sukkwono <sukkwono@sfu.ca>
2022-11-05 02:58:26 -0700

    personal changes

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -6,14 +6,46 @@ import IMG3 from '../../assets/portfolio3.jpg';
 import IMG4 from '../../assets/portfolio4.jpg';
 import IMG5 from '../../assets/portfolio5.png';
 import IMG6 from '../../assets/portfolio6.jpg';
+import {BsPatchCheckFill} from 'react-icons/bs';
+import {HiCheckCircle} from 'react-icons/hi';
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: 'Crypto',
-    github: "https://github.com"
+    title: 'Crypto Auto Trading',
+    github: "https://github.com",
+    stacks: ["Python", "Redis"]
+  },
+  {
+    id: 2,
+    image: IMG1,
+    title: 'Stock Reporter',
+    github: "https://github.com",
+    stacks: ["Python", "mongodb"]
+  },
+  {
+    id: 3,
+    image: IMG1,
+    title: 'Vancouver Room Rent App (IOS)',
+    github: "https://github.com",
+    stacks: ["Swift", "firebase"]
+  },
+  {
+    id: 4,
+    image: IMG1,
+    title: 'Starcraft Mocking Game',
+    github: "https://github.com",
+    stacks: ["Java"]
+  },
+  {
+    id: 5,
+    image: IMG1,
+    title: 'RPG Game',
+    github: "https://github.com",
+    stacks: ["C++"]
   }
+
 ]
 
 const Portfolio = () => {
@@ -24,13 +56,26 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         {
-          data.map(({id, image, title, github}) => {
+          data.map(({id, image, title, github, stacks}) => {
             return (
               <article key={id} className='portfolio__item'>
                 <div className='portfolio__item-image'>
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
+                <div className='fe'>
+                  {
+                    stacks.map((stacks) => {
+                      return (
+                        <article className='portfolio__stack'>
+                          <HiCheckCircle className='portfolio__stack-icon'/>
+                          <small className='text-light'>{stacks}</small>
+                        </article>
+                      )
+                    })
+                  }
+                </div>
+                
                 <div className='portfolio__item-cta'>
                   <a href={github} className='btn btn-primary'>Github</a>
                 </div>
