be6225736f44b80aaa60012c528e452a4d2004a1
sukkwono <sukkwono@sfu.ca>
2022-11-05 18:52:21 -0700

    small changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './header.css';
 import CTA from './CTA';
-import ME from '../../assets/me.png';
 import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
